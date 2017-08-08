577d2fc5a47a1fc20cb032d8a31fef08d31c1b8d
karyvdj <kaina51@hotmail.com>
2017-08-08 16:33:05 -0500

    Maquetacion Lista de restaurantes

--- js/detalleTakos.js
+++ js/detalleTakos.js
@@ -48,6 +48,7 @@ var restaurantes = [
 		"nombre": "Pizzas del Perro Negro",
 		"comida": "Pizzas",
 		"foto": "https://dummyimage.com/100x100",
+    "estrellas": 5,
 		"direccion": "Calle Orizaba 4, Local B, Cuauhtémoc, Roma Norte, 06700 Ciudad de México, CDMX",
     "coordenadas" : {"lat":"19.416512" ,"lng": "-99.169656"}
 	},
@@ -55,6 +56,7 @@ var restaurantes = [
 		"nombre": "Papa Guapa",
 		"comida": "Papas",
 		"foto": "https://dummyimage.com/100x100",
+    "estrellas": 4,
 		"direccion": "Calle Orizaba 4, Local B, Cuauhtémoc, Roma Norte, 06700 Ciudad de México, CDMX",
     "coordenadas" : {"lat":"19.419013" ,"lng": "-99.166943"}
 	},
@@ -62,6 +64,7 @@ var restaurantes = [
 		"nombre": "Barracuda Dinner",
 		"comida": "Hamburguesas",
 		"foto": "https://dummyimage.com/100x100",
+    "estrellas": 5,
 		"direccion": "Av Nuevo León 4-A, Condesa, Cuauhtémoc, 06140 Ciudad de México, CDMX.",
     "coordenadas" : {"lat":"19.415995" ,"lng": "-99.169623"}
 	},
@@ -69,6 +72,7 @@ var restaurantes = [
 		"nombre": "Moshi Moshi",
 		"comida": "Sushi",
 		"foto": "https://dummyimage.com/100x100",
+    "estrellas": 3,
 		"direccion": "Plaza Villa de Madrid 22, Cuauhtémoc, Roma, 06700 Ciudad de México, CDMX.",
     "coordenadas" : {"lat":"19.419407" ,"lng": "-99.166622"}
 	},
@@ -76,6 +80,7 @@ var restaurantes = [
 		"nombre": "Churrería El Moro",
 		"comida": "Churros y chocolate",
 		"foto": "https://dummyimage.com/100x100",
+    "estrellas": 3,
 		"direccion": "Eje Central Lázaro Cárdenas 42, Centro Histórico, Centro, 06000 Ciudad de México, CDMX",
     "coordenadas" : {"lat":"19.431437" ,"lng": "-99.141112"}
   }
